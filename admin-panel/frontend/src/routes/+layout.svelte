92e25d0cef1951d053fc611fb2808343136cfdc5
zach-sousa-dev <zacharysousa@outlook.com>
2025-02-11 21:57:47 -0500

    minor ui changes

--- admin-panel/frontend/src/routes/+layout.svelte
+++ admin-panel/frontend/src/routes/+layout.svelte
@@ -5,13 +5,16 @@
 	import NavButton from '$lib/components/NavButton.svelte';
 </script>
 
-<div class="m-auto bg-slate-700">
-	<nav class="2xl:max-w-[40%] xl:max-w-[50%] lg:max-w-[60%] sm:max-w-[100%] flex justify-between px-3">
-		<img src = {smallLogo} alt="4s solutions logo">
-		<NavButton text={"dashboard"} link={"#top"}/>
-		<NavButton text={"products"} link={"#top"}/>
-		<NavButton text={"analytics"} link={"#top"}/>
-	</nav>
-</div>
+<main class="max-w-[1500px] m-auto">
+	<div class="m-auto bg-slate-700">
+		<nav class="md:max-w-full lg:max-w-[60%] flex justify-between px-3">
+			<img src = {smallLogo} alt="4s solutions logo">
+			<NavButton text={"dashboard"} link={"#top"}/>
+			<NavButton text={"products"} link={"#top"}/>
+			<NavButton text={"analytics"} link={"#top"}/>
+		</nav>
+	</div>
+	{@render children()}
+</main>
 
-{@render children()}
+
