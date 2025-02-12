f2b22760898c46262114ff00d73f1ca8562eee12
zach-sousa-dev <zacharysousa@outlook.com>
2025-02-11 20:35:50 -0500

    added separate directory for admin panel, began working on frontend for admin panel

---
+++ admin-panel/frontend/src/lib/components/NavButton.svelte
@@ -0,0 +1,5 @@
+<script>
+    let { text, link } = $props();
+</script>
+
+<a class="uppercase text-xl md:text-3xl font-bold text-white py-2 transition-colors duration-300 hover:bg-slate-600" href={link}>{text}</a>
---
+++ admin-panel/frontend/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+	import smallLogo from '$lib/images/logoSmall.svg';
+	import NavButton from '$lib/components/NavButton.svelte';
+</script>
+
+<div class="m-auto bg-slate-700">
+	<nav class="2xl:max-w-[40%] xl:max-w-[50%] lg:max-w-[60%] sm:max-w-[100%] flex justify-between px-3">
+		<img src = {smallLogo} alt="4s solutions logo">
+		<NavButton text={"dashboard"} link={"#top"}/>
+		<NavButton text={"products"} link={"#top"}/>
+		<NavButton text={"analytics"} link={"#top"}/>
+	</nav>
+</div>
+
+{@render children()}
